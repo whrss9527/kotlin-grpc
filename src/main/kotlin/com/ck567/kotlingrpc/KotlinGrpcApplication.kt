7e022ac52c4e718b5d14805f28f8212d584789a1
Hao.Wu <1593774151@qq.com>
2023-07-07 15:08:41 +0800

    repo init

---
+++ src/main/kotlin/com/ck567/kotlingrpc/KotlinGrpcApplication.kt
@@ -0,0 +1,16 @@
+package com.ck567.kotlingrpc
+
+
+
+import io.grpc.ManagedChannelBuilder
+import vampire_ios.v1.AdminGetCommodityStatisticsReq
+import vampire_ios.v1.AdminGrpc
+
+
+fun main() {
+    val channel = ManagedChannelBuilder.forAddress("localhost", 8070).usePlaintext()
+
+    val stub = AdminGrpc.newBlockingStub(channel.build())
+    val response = stub.getCommodityStatistics(AdminGetCommodityStatisticsReq.newBuilder().setCommodityId("").setStartTime("0").setEndTime("111111111111111").build())
+    println(response)
+}
---
+++ src/test/kotlin/com/ck567/kotlingrpc/KotlinGrpcApplicationTests.kt
@@ -0,0 +1,13 @@
+//package com.ck567.kotlingrpc
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class KotlinGrpcApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
