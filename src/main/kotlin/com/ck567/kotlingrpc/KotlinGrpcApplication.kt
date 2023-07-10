b97d1b346ea66f9d63f5421e6798af257d59c66a
Hao.Wu <1593774151@qq.com>
2023-07-10 15:23:16 +0800

    update dependencies

--- src/main/kotlin/com/ck567/kotlingrpc/KotlinGrpcApplication.kt
+++ src/main/kotlin/com/ck567/kotlingrpc/KotlinGrpcApplication.kt
@@ -9,7 +9,6 @@ import vampire_ios.v1.AdminGrpc
 
 fun main() {
     val channel = ManagedChannelBuilder.forAddress("localhost", 8070).usePlaintext()
-
     val stub = AdminGrpc.newBlockingStub(channel.build())
     val response = stub.getCommodityStatistics(AdminGetCommodityStatisticsReq.newBuilder().setCommodityId("").setStartTime("0").setEndTime("111111111111111").build())
     println(response)
