1a09f77df329fd45abe784c4fbf645867ac89784
Hao.Wu <1593774151@qq.com>
2023-07-10 15:35:35 +0800

    clear no depend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import com.google.protobuf.gradle.*;
+import com.google.protobuf.gradle.*
 
 plugins {
     id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.asciidoctor.convert") version "1.5.9.2"
-    // 3.1.0 not support rest-docs
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
     id("com.google.protobuf") version "0.9.2"
@@ -36,14 +35,12 @@ extra["spring-restdocs.version"] = "2.0.5.BUILD-SNAPSHOT"
 val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
-    // tsv file reader
     implementation("com.univocity:univocity-parsers:2.9.1")
     implementation("com.google.protobuf:protobuf-java:3.22.2")
     implementation("io.grpc:grpc-protobuf:1.53.0")
     implementation("com.google.protobuf:protobuf-kotlin:3.22.2")
     implementation("io.grpc:grpc-kotlin-stub:1.3.0")
-    implementation("io.grpc:grpc-netty:1.46.0")
-    implementation("io.grpc:grpc-all:1.53.0")
+    implementation("io.grpc:grpc-netty:1.56.1")
 }
 
 protobuf {
